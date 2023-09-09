9671d26cdefdfef6777abd209feef16b99882d93
Damian Gutierrez Gaviria <damian41777@gmail.com>
2023-09-08 19:58:56 -0500

    JS check
    A espera de modificar

---
+++ script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    centeredSlides: true,
+    loop: true,
+    spaceBetween: 30,
+    grabCursor: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-next',
+    }
+})
